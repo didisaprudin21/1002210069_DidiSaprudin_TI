38832f1917298b52d97e9bda2e1fe642bec7843f
didi21 <dizdye@gmai.com>
2023-07-24 16:46:02 +0700

    first commit

---
+++ didi_saprudin.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>UAS DIDI SAPRUDIn</title>
+</head>
+<body>
+
+
+<p>Name : Didi Saprudin</p>
+<p>Nim : 1002210069</p>
+<p>Jurusan : Teknik Informatika</p>
+
+
+</body>
+</html>
